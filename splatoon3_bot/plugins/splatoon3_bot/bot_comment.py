0bd6649f15bacd7a62e8328437faa83033ee2f20
Paul <anyeccc@gmail.com>
2023-09-26 10:07:30 +0800

    1.3.7 微信支持留言板

--- splatoon3_bot/plugins/splatoon3_bot/bot_comment.py
+++ splatoon3_bot/plugins/splatoon3_bot/bot_comment.py
@@ -1,3 +1,4 @@
+from typing import Union
 from datetime import datetime as dt
 
 from nonebot import on_message, logger, on_regex
@@ -5,6 +6,7 @@ from nonebot.rule import to_me
 from nonebot.adapters import Event, Bot
 
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot as QQBot
+from nonebot.adapters.onebot.v12 import GroupMessageEvent as WXGME, Bot as WXBot
 
 from .db_sqlite import model_get_comment, model_add_comment
 from .utils import bot_send
@@ -26,52 +28,82 @@ async def _http(bot: QQBot, event: Event):
 
 @on_regex("^[\\/.,，。]?留言[板]?$", block=True).handle()
 async def _get_comment(bot: Bot, event: Event):
-    msg = await get_comment_table(bot_id=bot.self_id)
+    msg = await get_comment_table(bot)
     await bot_send(bot, event, msg, image_width=1000)
 
 
 @on_message(block=False, rule=to_me()).handle()
-async def _add_comment(bot: QQBot, event: GroupMessageEvent):
+async def _add_comment(bot: Union[QQBot, WXBot], event: Union[GroupMessageEvent, WXGME]):
     message = event.get_plaintext().strip() or '*'
-    if message[0] in ('/', '、', '.', '，', '。') or message == '*':
+    if message[0] in ('/', '、', '.', '，', '。') or message in ('*', '工', '全部工', '图', '全部图', '留言', '留言板'):
         return
 
     _event = event.dict() or {}
-    group_id = _event.get('group_id') or ''
-    try:
-        group_info = await bot.call_api('get_group_info', group_id=group_id)
-    except Exception as e:
-        logger.error(e)
-        group_info = {}
-    _dict = {
-        'user_id': event.get_user_id(),
-        'user_name': _event.get('sender', {}).get('nickname', ''),
-        'group_id': group_id,
-        'group_name': group_info.get('group_name', ''),
-        'message': message,
-    }
+    if isinstance(bot, QQBot):
+        group_id = _event.get('group_id') or ''
+        try:
+            group_info = await bot.call_api('get_group_info', group_id=group_id)
+        except Exception as e:
+            logger.error(e)
+            group_info = {}
+        _dict = {
+            'user_id': event.get_user_id(),
+            'user_name': _event.get('sender', {}).get('nickname', ''),
+            'group_id': group_id,
+            'group_name': group_info.get('group_name', ''),
+            'message': message,
+        }
+    else:
+        user_id = event.get_user_id()
+        user_info = await bot.get_user_info(user_id=event.get_user_id())
+        if not user_info:
+            logger.info(f'get_user_info wx failed, {event.get_user_id()}')
+            return
+        group_id = _event.get('group_id')
+        group_info = await bot.get_group_info(group_id=group_id) or {}
+
+        _dict = {
+            'bot_type': 'wx',
+            'user_id': user_id,
+            'user_icon': (user_info.get('wx') or {}).get('avatar') or '',
+            'user_name': user_info.get('user_name', ''),
+            'group_id': group_id,
+            'group_name': group_info.get('group_name', ''),
+            'group_icon': (group_info.get('wx') or {}).get('avatar') or '',
+            'message': message,
+        }
+
     model_add_comment(**_dict)
 
-    msg = await get_comment_table(bot_id=bot.self_id)
+    msg = await get_comment_table(bot)
     await bot_send(bot, event, msg, image_width=1000)
 
 
-async def get_comment_table(bot_id):
+async def get_comment_table(bot):
     comment_lst = model_get_comment() or []
 
-    msg = f'''#### 留言板({bot_id}) HKT {dt.now():%Y-%m-%d %H:%M:%S}
+    msg = f'''#### 留言板 HKT {dt.now():%Y-%m-%d %H:%M:%S}
 ||||||
 |---:||---:||---|
 '''
     for c in comment_lst[-30:]:
         user_name = c.user_name or ''
-        user_name += f''' |<img height="40" src="https://q1.qlogo.cn/g?b=qq&nk={c.user_id}&s=640"/>'''
+        u_icon = f"https://q1.qlogo.cn/g?b=qq&nk={c.user_id}&s=640"
+        if c.bot_type == 'wx':
+            u_icon = c.user_icon or ''
+            user_name += ' V'
+        user_name += f''' |<img height="40" src="{u_icon}"/>'''
         group_name = c.group_name or ''
         if group_name:
-            group_name += f'''| <img height="40" src="https://p.qlogo.cn/gh/{c.group_id}/{c.group_id}/100"/>'''
+            g_icon = f"https://p.qlogo.cn/gh/{c.group_id}/{c.group_id}/100"
+            if c.bot_type == 'wx':
+                g_icon = c.group_icon or ''
+            group_name += f'''| <img height="40" src="{g_icon}"/>'''
         else:
             group_name = '|'
         cmt = c.message.strip().replace('\n', ' ').replace('|', '\|')
+        if isinstance(bot, WXBot):
+            cmt = cmt[:30]
         msg += f"|{group_name}|{user_name}|{cmt}|\n"
 
     page = ''
