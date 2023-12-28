7fe2ae37fb123a36a78ba47b142ba65ce2528e32
Paul <anyeccc@gmail.com>
2023-12-28 08:55:56 +0800

    fixed FileNotFoundError weapon R-PEN/5H

--- splatoon3_bot/plugins/splatoon3_nso/db_sqlite.py
+++ splatoon3_bot/plugins/splatoon3_nso/db_sqlite.py
@@ -248,6 +248,8 @@ def GetInsertOrUpdateObj(cls, strFilter, **kw):
 
 async def model_get_or_set_temp_image(_type, name, link) -> TempImageTable:
     """获取或设置缓存图片"""
+    # FileNotFoundError weapon R-PEN/5H
+    name = name.replace('/', '_')
     session = DBSession()
     row: TempImageTable = session.query(TempImageTable).filter(
         (TempImageTable.type == _type) & (TempImageTable.name == name)).first()
@@ -255,7 +257,7 @@ async def model_get_or_set_temp_image(_type, name, link) -> TempImageTable:
     temp_image = TempImageTable()
     if row:
         # 判断是否是用户图像缓存，并比对缓存数据是否需要更新
-        if (row.type == "friend_icon" and row.link != link) or (row.type == "ns_friend_icon" and row.link != link) or (row.type == "my_icon" and row.link != link):
+        if row.type in ("friend_icon", 'ns_friend_icon', 'my_icon') and row.link != link:
             download_flag = True
         else:
             temp_image = copy.deepcopy(row)
